2e2104954f40394eca13f69d1b04f86c1a3ee1bf
Harsh <bhonsleharsh16@gmail.com>
2024-12-05 18:45:47 +0530

    setup routes for user

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,26 @@
 import {Router} from "express"
 import {registerUser} from "../controllers/user.controllers.js"
-
+import {upload} from "../middlewares/multer.middleware.js"
 const router = Router();
 
-router.route("/register").post(registerUser)
+// router.route("/register").post(registerUser)
+// new syntax 
+// router.get('/register',(req,res)=>{
+//     res.json({message:'ok'})
+// })
 
+router.post('/register',
+    upload.fields([
+        {
+            name:"avatar",
+            maxCount:1
+        },
+        {
+            name:"coverImage",
+            maxCount:1
+        }
+    ]),
+    registerUser)
 
 export default router
 
