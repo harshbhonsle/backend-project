2d0b53755e4ddc4a34cf780dd5a4cd6e897f81bd
Harsh <bhonsleharsh16@gmail.com>
2025-03-31 00:49:35 +0530

    Add logIn and logOut

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import {Router} from "express"
-import {registerUser} from "../controllers/user.controllers.js"
+import {loginUser, logoutUser, registerUser} from "../controllers/user.controllers.js"
 import {upload} from "../middlewares/multer.middleware.js"
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router();
 
 // router.route("/register").post(registerUser)
@@ -8,7 +9,7 @@ const router = Router();
 // router.get('/register',(req,res)=>{
 //     res.json({message:'ok'})
 // })
-
+// upload aaya hai multer and it gives us many options
 router.post('/register',
     upload.fields([
         {
@@ -17,10 +18,15 @@ router.post('/register',
         },
         {
             name:"coverImage",
-            maxCount:1
+            maxCount:1 
         }
     ]),
     registerUser)
+
+    router.route("/login").post(loginUser)
+
+    // secured routes
+    router.route("/loggout").post(verifyJWT, logoutUser,)
 
 export default router
 
