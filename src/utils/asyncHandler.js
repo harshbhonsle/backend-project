76e655cfa4a0cb65bfdb15fa8a8949344759f383
Harsh <bhonsleharsh16@gmail.com>
2025-03-31 00:51:38 +0530

    mod: fix typo and minor changes

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,7 +1,20 @@
-const asyncHandler = (handlerRequest) =>{
-   return (req,res,next)=>{
-        Promise.resolve(req,res,next).catch((err)=>next(err));
+const asyncHandler = (requestHandler) => {
+    return (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
     }
 }
 
-export {asyncHandler}
+
+export { asyncHandler }
+
+
+
+
+
+// const asyncHandler = (handlerRequest) =>{
+//    return (req,res,next)=>{
+//         Promise.resolve(req,res,next).catch((err)=>next(err));
+//     }
+// }
+
+// export {asyncHandler}
